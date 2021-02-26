7fff62a6d61cc39783723b346652016d38d415ea
Paola Nakasone Nakahodo <jpaola.nakasone@gmail.com>
2021-02-26 11:13:43 -0500

    Authentication - Hashing and Salting

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,9 +7,8 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+
+    public class UsersController : BaseApiController
 
     {
         private readonly DataContext _context;
