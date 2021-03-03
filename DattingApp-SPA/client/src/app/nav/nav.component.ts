3f7b4a16440cae423130232301f2329c1dc731db
Paola Nakasone Nakahodo <jpaola.nakasone@gmail.com>
2021-03-02 20:30:53 -0500

    Using Angular templates Forms - Angular services

--- DattingApp-SPA/client/src/app/nav/nav.component.ts
+++ DattingApp-SPA/client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  model: any = {}
+  loggedIn: boolean = false;
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      this.loggedIn = true;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
