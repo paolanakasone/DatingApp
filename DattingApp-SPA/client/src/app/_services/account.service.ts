6f848cbf213f5058a9e7301b32dc7cf3b9a23bcb
Paola Nakasone Nakahodo <jpaola.nakasone@gmail.com>
2021-03-05 21:25:09 -0500

    Routing in Angular & Notification

--- DattingApp-SPA/client/src/app/_services/account.service.ts
+++ DattingApp-SPA/client/src/app/_services/account.service.ts
@@ -43,6 +43,6 @@ export class AccountService {
 
   logout() {
     localStorage.removeItem('user');
-    this.currentUserSource.next(undefined);
+    this.currentUserSource.next(undefined)
   }
 }
--- DattingApp-SPA/client/src/app/nav/nav.component.ts
+++ DattingApp-SPA/client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -11,21 +13,25 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
+      this.router.navigateByUrl('/members');
       console.log(response);
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 
