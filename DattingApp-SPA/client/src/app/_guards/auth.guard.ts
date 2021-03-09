670eb127fd9e60cad4efb96fe9c14be4a6459f69
Paola Nakasone Nakahodo <jpaola.nakasone@gmail.com>
2021-03-08 20:27:23 -0500

    Bootstrap theme / Angular route guards / Shared Module

---
+++ DattingApp-SPA/client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/internal/operators/map';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    throw new Error('Method not implemented.');
+  }
+/* 
+  canActivate(): Observable<boolean> {
+   return this.accountService.currentUser$.pipe(
+      map(user => {    
+        if (user) return true
+        this.toastr.error('You shall not pass!');
+      })
+    );
+    
+  }
+  */
+  
+}
--- DattingApp-SPA/client/src/app/app.module.ts
+++ DattingApp-SPA/client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -33,11 +34,8 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    FormsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right'
-    })
+    FormsModule   ,
+    SharedModule
    ],
   providers: [],
   bootstrap: [AppComponent]
