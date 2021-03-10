68c25884ec8fd5ad6426881ff2dcb202d0a4544a
Paola Nakasone Nakahodo <jpaola.nakasone@gmail.com>
2021-03-09 20:20:06 -0500

    Handling server errors

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        private readonly IHostEnvironment _env;
+
+      /*  public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
+            IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+        }
+      */
+    }
+}
+
+
--- API/Startup.cs
+++ API/Startup.cs
@@ -43,11 +43,11 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+        /*    if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+        */
             app.UseHttpsRedirection();
 
             app.UseRouting();
