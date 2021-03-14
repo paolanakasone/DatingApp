b2e01cc730d212658fd83003eade388509ce51a2
Paola Nakasone Nakahodo <jpaola.nakasone@gmail.com>
2021-03-13 19:44:52 -0500

    Extending the API - part 1

---
+++ API/Data/Seed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            if (await context.Users.AnyAsync()) return;
+
+        }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,7 @@
+using API.Extensions;
+using System;
+using System.Collections.Generic;
+
 namespace API.Entities
 {
     public class AppUser
@@ -9,6 +13,33 @@ namespace API.Entities
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnowAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookigFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
 
     }
 }
